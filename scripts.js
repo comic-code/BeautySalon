8faaf59d653c64f9a9ea06c8982c931df256a598
comic <bitotajima@hotmail.com>
2022-02-16 15:31:07 -0300

    ✅ - Menu marcado de acordo com section

--- scripts.js
+++ scripts.js
@@ -14,8 +14,30 @@ for (const link of links) {
 const header = document.querySelector('.mainHeader')
 const navHeight = header.offsetHeight
 const backToTopButton = document.querySelector('.back-to-top')
+const sections = document.querySelectorAll('main section[id]')
 
 window.addEventListener('scroll', () => {
+  const checkpoint = window.scrollY + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector(`nav ul li a[href*="${sectionId}"`)
+        .classList.add('active')
+    } else {
+      document
+        .querySelector(`nav ul li a[href*="${sectionId}"`)
+        .classList.remove('active')
+    }
+  }
+
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -46,7 +68,6 @@ const swiper = new Swiper('.swiper', {
   }
 })
 
-// ScrollReveal
 const scrollReveal = ScrollReveal({
   origin: 'top',
   distance: '30px',
